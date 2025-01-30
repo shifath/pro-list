4a721bd1343c4155ad1643ef2e823942b51454d0
Shifath <shifath.shabbib@gmail.com>
2025-01-30 18:15:17 +0600

    hello world

--- app.js
+++ app.js
@@ -2,6 +2,5 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(<h1>Hello World</h1>));
-
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get("/", (req, res) => res.type('html').send("Hello World"));
